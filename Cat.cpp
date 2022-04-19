9a7a2c0673c7d2d8f5d0b840bf33551e9ce4b6c6
reidlum <reidlum@hawaii.edu>
2022-04-18 23:14:18 -1000

    made Cat.cpp code

--- Cat.cpp
+++ Cat.cpp
@@ -12,179 +12,45 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdexcept>
-#include <iomanip>
-#include <cassert>
+
 
 #include "catDatabase.h"
 #include "Cat.h"
 #include "config.h"
 
+const std::string Cat::SPECIES_NAME = "Felis Catus";
+const Weight::t_weight Cat::MAX_WEIGHT = 40;
 
-using namespace std;
-
-Cat::Cat() {
-    zeroOutMemberData();
+std::string Cat::getName() const noexcept {
+    return Cat::name;
 }
 
-Cat::~Cat() {
-    zeroOutMemberData();
-}
-
-Cat::Cat(const char *newName, Gender inputGender, Breed inputBreed, Weight inputWeight) : Cat() {
-    setName(newName);
-    setGender(inputGender);
-    setBreed(inputBreed);
-    setWeight(inputWeight);
-
-}
-
-const char *Cat::getName() const {
-    return name;
-}
-
-Gender Cat::getGender() const {
-    return gender;
-}
-
-Breed Cat::getBreed() const {
-    return breed;
-}
-
-bool Cat::isFixed() const {
-    return isCatFixed;
-}
-
-Weight Cat::getWeight() const {
-    return weight;
-}
-
-void Cat::setName(const char *newName) {
-    if (validateName(newName))
-    {
-        memset(name, 0, MAX_CAT_NAME);
-        strcpy(name, newName);
+void Cat::setName(const std::string &newName) {
+    if (validateName(newName)){
+        Cat::name = newName;
     }
 }
 
-void Cat::setGender(Gender inputGender) {
-    if (gender == UNKNOWN_GENDER)
-    {
-        validateGender(inputGender);
-        Cat::gender = inputGender;
+bool Cat::isFixed() const noexcept {
+    if (Cat::isCatFixed){
+        return true;
     }
-    else{
-        fprintf( stderr, "%s: No transgender cats.\n", PROGRAM_TITLE) ;
-    }
+    return false;
 }
 
-void Cat::setBreed(Breed inputBreed) {
-    if (breed == UNKNOWN_BREED)
-    {
-        validateBreed(inputBreed);
-        Cat::breed = inputBreed;
-    }
-    else{
-        fprintf( stderr, "%s: No transbreed cats.\n", PROGRAM_TITLE) ;
-    }
-}
-
-void Cat::fixCat() {
+void Cat::fixCat() noexcept {
     Cat::isCatFixed = true;
 }
 
-void Cat::setWeight(Weight inputWeight) {
-    if (validateWeight(inputWeight))
-    {
-        Cat::weight = inputWeight;
-    }
+std::string Cat::speak() const noexcept {
+    return "Meow";
 }
 
-bool Cat::validateName(const char *newName) {
-    if (newName == nullptr) //checks if newName is nullptr
-    {
-        fprintf( stderr, "%s: Cat name [%s] can't be NULL.\n", PROGRAM_TITLE, newName ) ;
-        return false;
-    }
-
-    if (strlen(newName) <= 0)
-    {
-        fprintf( stderr, "%s: Cat name [%s] can't be <= 0.\n", PROGRAM_TITLE, newName ) ;
-        return false;
-    }
-
-    if (strlen(newName) >= MAX_CAT_NAME) //Checks if cat name is < 30
-    {
-        fprintf(stderr, "%s: Cat name [%s] is too long.\n", PROGRAM_TITLE, newName);
+bool Cat::validateName(const std::string &newName) {
+    if (newName.empty()){
         return false;
     }
     return true;
-}
-
-bool Cat::validateGender(const Gender inputGender) {
-    if (inputGender == UNKNOWN_GENDER)
-    {
-        fprintf( stderr, "%s: Gender must be known.\n", PROGRAM_TITLE, inputGender ) ;
-        return false;
-    }
-    return true;
-}
-
-bool Cat::validateBreed(const Breed inputBreed) {
-    if (inputBreed == UNKNOWN_BREED)
-    {
-        fprintf( stderr, "%s: Breed must be known.\n", PROGRAM_TITLE, inputBreed ) ;
-        return false;
-    }
-    return true;
-}
-
-bool Cat::validateWeight(const Weight inputWeight) {
-    if (inputWeight <= 0) //Checks if weight is > 0
-    {
-        fprintf( stderr, "%s: Cat weight [%f] isn't possible.\n", PROGRAM_TITLE, inputWeight ) ;
-        return false;
-    }
-    return true;
-}
-
-void Cat::zeroOutMemberData() {
-    memset(name, 0, MAX_CAT_NAME);
-    gender = UNKNOWN_GENDER;
-    breed = UNKNOWN_BREED;
-    isCatFixed = false;
-    weight = UNKNOWN_WEIGHT;
-    next = nullptr;
-}
-
-
-/// Format a line for printing the members of a class
-#define FORMAT_LINE( className, member ) cout << setw(8) << (className) << setw(20) << (member) << setw(52)
-/// @returns true if everything worked correctly. false if something goes
-/// wrong
-
-bool Cat::print() const noexcept{
-    assert( validate() ) ;
-    cout << setw(80) << setfill( '=' ) << "" << endl ;
-    cout << setfill( ' ' ) ;
-    cout << left ;
-    cout << boolalpha ;
-    FORMAT_LINE( "Cat", "name" ) << getName() << endl ;
-    FORMAT_LINE( "Cat", "gender" ) << genderName( getGender() ) << endl ;
-    FORMAT_LINE( "Cat", "breed" ) << breedName( getBreed() ) << endl ;
-    FORMAT_LINE( "Cat", "isFixed" ) << isFixed() << endl ;
-    FORMAT_LINE( "Cat", "weight" ) << getWeight() << endl ;
-    return true ;
-}
-
-
-bool Cat::validate() const noexcept{
-    if(validateName( name ) && validateGender(gender) && validateBreed(breed) && validateWeight(weight))
-    {
-        return true;
-    }
-    else{
-        return false;
-    }
 }
 
 
